c5caeb77b978fd4bd7a2c29690f52f5276f2f713
unknown <sasjoy02@gmail.com>
2023-08-15 22:13:06 -0700

    deposit and withdraw field add

--- js/login.js
+++ js/login.js
@@ -1,4 +1,16 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
     const emailField = document.getElementById('email-field');
+    const email = emailField.value;
+    const passwordField = document.getElementById('password');
+    const password = passwordField.value;
+    emailField.value = '';
+    passwordField.value = '';
+    if(email === 'sasjoy02@gmail.com' && password === '01764746891'){
+        window.location = 'bank.html'
+    }
+    else{
+        alert('Toke ami tejjo sontan ghosona korlam tui password vule gechos')
+    }
+
     
 })
